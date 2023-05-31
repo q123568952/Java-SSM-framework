bbdb85b7cd6227baafe2d820f5c2b71b83120c07
Morgan Huang <m800313@gmail.com>
2023-05-31 09:47:10 +0800

    initial spring-IoC-and-di-demo project

---
+++ spring-IoC-and-di-demo/src/main/java/com/morgan/spring/IoC/and/di/demo/SpringIoCAndDiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.morgan.spring.IoC.and.di.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoCAndDiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoCAndDiDemoApplication.class, args);
+	}
+
+}
---
+++ spring-IoC-and-di-demo/src/test/java/com/morgan/spring/IoC/and/di/demo/SpringIoCAndDiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morgan.spring.IoC.and.di.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoCAndDiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

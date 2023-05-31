789abd5a775949ed01879d545c61abf8fc1ca5e5
Morgan Huang <m800313@gmail.com>
2023-05-31 09:51:33 +0800

    tune folder structure

---
+++ spring-IoC-and-di-demo/src/test/java/com/morgan/springIoCandDIdemo/SpringIoCAndDiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morgan.springIoCandDIdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoCAndDiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

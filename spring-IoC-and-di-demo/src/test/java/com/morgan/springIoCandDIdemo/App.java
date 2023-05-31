d91ac29ef7d6acc375270c67e22777b719553524
Morgan Huang <m800313@gmail.com>
2023-05-31 10:17:50 +0800

    basic modification for the onineclass

--- spring-IoC-and-di-demo/src/test/java/com/morgan/springIoCandDIdemo/App.java
+++ spring-IoC-and-di-demo/src/test/java/com/morgan/springIoCandDIdemo/App.java
@@ -1,12 +1,9 @@
 package com.morgan.springIoCandDIdemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class App {
-
-	@Test
 	void contextLoads() {
 	}
 

6e5258e06a12bfee2d46265069b3c9da0382a885
Morgan Huang <m800313@gmail.com>
2023-06-02 00:03:13 +0800

    using implements to define lifecycle and show the order

--- spring-bean-lifecycle/src/main/java/com/morgan/springIoCandDIdemo/service/impl/BookServiceImpl.java
+++ spring-bean-lifecycle/src/main/java/com/morgan/springIoCandDIdemo/service/impl/BookServiceImpl.java
@@ -2,9 +2,13 @@ package com.morgan.springIoCandDIdemo.service.impl;
 
 
 import com.morgan.springIoCandDIdemo.service.BookService;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 import com.morgan.springIoCandDIdemo.dao.BookDao;
 
-public class BookServiceImpl implements BookService{
+public class BookServiceImpl implements BookService, InitializingBean, DisposableBean{
 
     private BookDao BookDao;
 
@@ -14,7 +18,18 @@ public class BookServiceImpl implements BookService{
     }
 
     public void setBookDao(BookDao bookDao) {
+        System.out.println("set bookdao ...");
         BookDao = bookDao;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("service destroy ...");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("service init ...");
     }
     
 }

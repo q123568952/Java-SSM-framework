32bca665f0f6254bdbb9a03125a61083a08468c7
Morgan Huang <m800313@gmail.com>
2023-06-01 21:06:26 +0800

    setup lifecycle demo code

---
+++ spring-bean-lifecycle/src/main/java/com/morgan/springIoCandDIdemo/Appd202.java
@@ -0,0 +1,18 @@
+package com.morgan.springIoCandDIdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.morgan.springIoCandDIdemo.service.BookService;
+
+public class Appd202 {
+
+	public static void main(String[] args) {
+		// get bean
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// BookDao bookDao = (BookDao)ctx.getBean("bookDao");
+		// bookDao.save();
+		BookService bookService = (BookService)ctx.getBean("service2");
+		bookService.save();
+	}
+
+}

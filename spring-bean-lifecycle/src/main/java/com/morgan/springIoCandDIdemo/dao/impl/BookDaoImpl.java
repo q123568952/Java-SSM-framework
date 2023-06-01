154d661431e6cb42868b3a35362b671924fc8894
Morgan Huang <m800313@gmail.com>
2023-06-01 23:52:49 +0800

    add lifecycle define

--- spring-bean-lifecycle/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
+++ spring-bean-lifecycle/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
@@ -7,4 +7,10 @@ public class BookDaoImpl implements BookDao{
     public void save(){
         System.out.println("book dao save ...");
     }
+    public void init(){
+        System.out.println("init ...");
+    }
+    public void destroy(){
+        System.out.println("destroy ...");
+    }
 }

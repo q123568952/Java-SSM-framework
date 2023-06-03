30b9e5e0f70414aaa67fb103e757c8e9cd0bbaae
Morgan Huang <m800313@gmail.com>
2023-06-03 18:06:45 +0800

    add  userDao parameter in bookserviceimpl

--- spring-setter/src/main/java/com/morgan/springIoCandDIdemo/service/impl/BookServiceImpl.java
+++ spring-setter/src/main/java/com/morgan/springIoCandDIdemo/service/impl/BookServiceImpl.java
@@ -1,24 +1,28 @@
 package com.morgan.springIoCandDIdemo.service.impl;
-
-
 import com.morgan.springIoCandDIdemo.service.BookService;
-
-
 import com.morgan.springIoCandDIdemo.dao.BookDao;
+import com.morgan.springIoCandDIdemo.dao.UserDao;
 
 public class BookServiceImpl implements BookService{
 
-    private BookDao BookDao;
-    private UserDao UserDao;
+    private BookDao bookDao;
+    private UserDao userDao;
 
     public void save(){
         System.out.println("book service save ...");
-        BookDao.save();
+        bookDao.save();
+        userDao.save();
     }
 
+
+    
     public void setBookDao(BookDao bookDao) {
         System.out.println("set bookdao ...");
-        BookDao = bookDao;
+        this.bookDao = bookDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
     }
 
     

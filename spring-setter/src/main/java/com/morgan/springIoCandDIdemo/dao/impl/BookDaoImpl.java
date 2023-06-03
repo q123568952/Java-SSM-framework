2d6e982dec698757b895c4f5328c8d01a96b1fa3
Morgan Huang <m800313@gmail.com>
2023-06-03 20:04:13 +0800

    modify for simple bean property setter

--- spring-setter/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
+++ spring-setter/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
@@ -3,14 +3,19 @@ package com.morgan.springIoCandDIdemo.dao.impl;
 import com.morgan.springIoCandDIdemo.dao.BookDao;
 
 public class BookDaoImpl implements BookDao{
+
+    private int connectionNum;
+    private String databaseName;
     
+    
+    public void setConnectionNum(int connectionNum) {
+        this.connectionNum = connectionNum;
+    }
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
     public void save(){
-        System.out.println("book dao save ...");
+        System.out.println("book dao save ..." + databaseName +"," + connectionNum);
     }
-    public void init(){
-        System.out.println("init ...");
-    }
-    public void destroy(){
-        System.out.println("destroy ...");
-    }
+
 }

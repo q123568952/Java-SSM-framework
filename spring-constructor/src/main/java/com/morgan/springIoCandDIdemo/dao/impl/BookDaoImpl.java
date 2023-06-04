c8ee1bf3b38254879ce57b85ca3a5f5dd3886292
Morgan Huang <m800313@gmail.com>
2023-06-04 08:19:58 +0800

    using constructor to do DI

--- spring-constructor/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
+++ spring-constructor/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
@@ -6,7 +6,16 @@ public class BookDaoImpl implements BookDao{
 
     private int connectionNum;
     private String databaseName;
+
     
+    
+    public BookDaoImpl(int connectionNum, String databaseName) {
+        this.connectionNum = connectionNum;
+        this.databaseName = databaseName;
+    }
+
+
+
     public void save(){
         System.out.println("book dao save ..." + databaseName +"," + connectionNum);
     }
--- spring-constructor/src/main/java/com/morgan/springIoCandDIdemo/service/impl/BookServiceImpl.java
+++ spring-constructor/src/main/java/com/morgan/springIoCandDIdemo/service/impl/BookServiceImpl.java
@@ -6,18 +6,19 @@ import com.morgan.springIoCandDIdemo.dao.UserDao;
 public class BookServiceImpl implements BookService{
 
     private BookDao bookDao;
+    private UserDao userDao;
+
+
+    
+    public BookServiceImpl(BookDao bookDao, UserDao userDao) {
+        this.bookDao = bookDao;
+        this.userDao = userDao;
+    }
+
 
     public void save(){
         System.out.println("book service save ...");
         bookDao.save();
-    }
-
-
-    
-    public void setBookDao(BookDao bookDao) {
-        this.bookDao = bookDao;
-    }
-
-    
-    
+        userDao.save();
+    }  
 }

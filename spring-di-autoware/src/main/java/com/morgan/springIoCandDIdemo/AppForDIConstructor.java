e740dbbc5e9bff1a16039c1f40a59890628a0696
Morgan Huang <m800313@gmail.com>
2023-06-04 08:54:42 +0800

    initial online class demo code for autoware demo

---
+++ spring-di-autoware/src/main/java/com/morgan/springIoCandDIdemo/AppForDIConstructor.java
@@ -0,0 +1,21 @@
+package com.morgan.springIoCandDIdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.morgan.springIoCandDIdemo.service.BookService;
+
+public class AppForDIConstructor {
+
+	public static void main(String[] args) {
+		// get bean
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ctx.registerShutdownHook();
+		// BookDao bookDao = (BookDao)ctx.getBean("bookDao");
+		// bookDao.save();
+		BookService bookService = (BookService)ctx.getBean("bookService");
+		bookService.save();
+		
+		// ctx.close();
+	}
+
+}

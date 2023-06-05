660068bb3990524491a50eea5f0d7bc8fcd48d4e
Morgan Huang <m800313@gmail.com>
2023-06-05 09:15:02 +0800

    using filesystemxmlapplicationcontext to upload applicationcontext

--- spring-container/src/main/java/com/morgan/springIoCandDIdemo/App.java
+++ spring-container/src/main/java/com/morgan/springIoCandDIdemo/App.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.morgan.springIoCandDIdemo.dao.BookDao;
 
@@ -12,9 +13,8 @@ public class App {
 
 	public static void main(String[] args) {
 		// get bean
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		DataSource dataSource = (DataSource) ctx.getBean("dataSource");
-		System.out.println(dataSource);
+		// ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("D:/springlearning/Java-SSM-framework/spring-container/src/main/resources/applicationContext.xml");
 		BookDao bookDao =(BookDao) ctx.getBean("bookDao");
 		bookDao.save();
 

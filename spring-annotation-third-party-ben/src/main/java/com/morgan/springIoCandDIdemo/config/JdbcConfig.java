bfb6dcffe33236dde9ce5857338e0b7c7ebdd948
Morgan Huang <m800313@gmail.com>
2023-06-05 13:34:57 +0800

    using @value to alternate parameter

--- spring-annotation-third-party-ben/src/main/java/com/morgan/springIoCandDIdemo/config/JdbcConfig.java
+++ spring-annotation-third-party-ben/src/main/java/com/morgan/springIoCandDIdemo/config/JdbcConfig.java
@@ -2,19 +2,29 @@ package com.morgan.springIoCandDIdemo.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.alibaba.druid.pool.DruidDataSource;
 
 public class JdbcConfig {
 
+    @Value("com.mysql.jdbc.Driver")
+    private String driver;  
+    @Value("jdbc:mysql://localhost:3306/spring_db")
+    private String url;
+    @Value("root")
+    private String username;
+    @Value("root")
+    private String password;
+
     @Bean
     public DataSource dataSource(){
         DruidDataSource ds = new DruidDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/spring_db");
-        ds.setUsername("root");
-        ds.setPassword("root");
+        ds.setDriverClassName(driver);
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
         return ds;
     }
 }

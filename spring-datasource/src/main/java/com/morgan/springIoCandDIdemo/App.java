ff5ff76699e74d44b86c2150571fcc201076b425
Morgan Huang <m800313@gmail.com>
2023-06-05 08:57:41 +0800

    using property file to define parameter step 3 test

--- spring-datasource/src/main/java/com/morgan/springIoCandDIdemo/App.java
+++ spring-datasource/src/main/java/com/morgan/springIoCandDIdemo/App.java
@@ -5,6 +5,8 @@ import javax.sql.DataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.morgan.springIoCandDIdemo.dao.BookDao;
+
 
 public class App {
 
@@ -13,6 +15,8 @@ public class App {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		DataSource dataSource = (DataSource) ctx.getBean("dataSource");
 		System.out.println(dataSource);
+		BookDao bookDao =(BookDao) ctx.getBean("bookDao");
+		bookDao.save();
 
 	}
 
--- spring-datasource/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
+++ spring-datasource/src/main/java/com/morgan/springIoCandDIdemo/dao/impl/BookDaoImpl.java
@@ -4,21 +4,14 @@ import com.morgan.springIoCandDIdemo.dao.BookDao;
 
 public class BookDaoImpl implements BookDao{
 
-    private int connectionNum;
-    private String databaseName;
-
-
-
-
-    public void setConnectionNum(int connectionNum) {
-        this.connectionNum = connectionNum;
-    }
-    public void setDatabaseName(String databaseName) {
-        this.databaseName = databaseName;
-    }
+    private String name;
 
     public void save(){
-        System.out.println("book dao save ..." + databaseName +"," + connectionNum);
+        System.out.println("book dao save ..." + name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }

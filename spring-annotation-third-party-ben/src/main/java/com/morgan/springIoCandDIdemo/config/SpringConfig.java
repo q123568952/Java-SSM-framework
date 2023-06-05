0ddad6f40a5da43f22e866312b619cb312dffa61
Morgan Huang <m800313@gmail.com>
2023-06-05 13:09:24 +0800

    initial code for annotation third party source di

---
+++ spring-annotation-third-party-ben/src/main/java/com/morgan/springIoCandDIdemo/config/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.morgan.springIoCandDIdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+public class SpringConfig {
+    
+    
+}

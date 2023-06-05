ae8a39060033baa8435fdd4e67a798d6b2bbfb58
Morgan Huang <m800313@gmail.com>
2023-06-05 13:14:07 +0800

    initial code and add @bean demo code

--- spring-annotation-third-party-ben/src/main/java/com/morgan/springIoCandDIdemo/config/SpringConfig.java
+++ spring-annotation-third-party-ben/src/main/java/com/morgan/springIoCandDIdemo/config/SpringConfig.java
@@ -1,11 +1,24 @@
 package com.morgan.springIoCandDIdemo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import com.alibaba.druid.pool.DruidDataSource;
+
 @Configuration
 public class SpringConfig {
-    
-    
+
+    @Bean
+    public DataSource dataSource(){
+        DruidDataSource ds = new DruidDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/spring_db");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        return ds;
+    }
 }

a8d5a7e2951b0898f697b9307db82fadc4c9d8d4
Morgan Huang <m800313@gmail.com>
2023-06-07 08:55:48 +0800

    add dependency for integrating mybatis

---
+++ spring-mybatis/src/main/java/com/morgan/springIoCandDIdemo/App.java
@@ -0,0 +1,12 @@
+package com.morgan.springIoCandDIdemo;
+
+import com.morgan.springIoCandDIdemo.service.BookService;
+import com.morgan.springIoCandDIdemo.service.impl.BookServiceImpl;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	}
+
+}

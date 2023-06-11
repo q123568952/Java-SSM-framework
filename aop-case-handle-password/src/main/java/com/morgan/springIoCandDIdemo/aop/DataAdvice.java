3ae95c495560e31b1727815c8e510a3317684b2c
Morgan Huang <m800313@gmail.com>
2023-06-11 10:52:03 +0800

    finish password and url trim function

---
+++ aop-case-handle-password/src/main/java/com/morgan/springIoCandDIdemo/aop/DataAdvice.java
@@ -0,0 +1,29 @@
+package com.morgan.springIoCandDIdemo.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class DataAdvice {
+
+    @Pointcut("execution(boolean com.morgan.springIoCandDIdemo.service.*Service.openURL(*,*))")
+    private void Pt(){};
+
+    @Around("DataAdvice.Pt()")
+    public Object trimStr(ProceedingJoinPoint pjp) throws Throwable{
+        Object[] args = pjp.getArgs();
+        for (int i = 0; i < args.length; i++) {
+            if(args[i].getClass().equals(String.class)){
+                args[i]= args[i].toString().trim();
+            };
+        }
+        Object ret = pjp.proceed(args);
+
+        return ret;
+    }
+    
+}

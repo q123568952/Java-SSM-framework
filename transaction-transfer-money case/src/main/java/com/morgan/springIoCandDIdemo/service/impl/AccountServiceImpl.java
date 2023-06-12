b7641df43ab7d9fe88b96defe5e0007d12490be1
Morgan Huang <m800313@gmail.com>
2023-06-12 10:39:02 +0800

    add error in serviceimpl to confirm transaction demo

--- transaction-transfer-money case/src/main/java/com/morgan/springIoCandDIdemo/service/impl/AccountServiceImpl.java
+++ transaction-transfer-money case/src/main/java/com/morgan/springIoCandDIdemo/service/impl/AccountServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.morgan.springIoCandDIdemo.dao.AccountDao;
 import com.morgan.springIoCandDIdemo.domain.Account;
@@ -16,10 +17,11 @@ public class AccountServiceImpl implements AccountService{
     @Autowired
     private AccountDao accountDao;
 
-
+    
     @Override
     public void transfer(String out, String in, Double money) {
         accountDao.inMoney(in, money);
+        // int i = 1/0; 
         accountDao.outMoney(out, money);
     }
 

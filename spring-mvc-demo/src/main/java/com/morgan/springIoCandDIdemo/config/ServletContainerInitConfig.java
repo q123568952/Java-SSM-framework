1d1e0e35651787f3c02fb2029de4b4acaa96d865
Morgan Huang <m800313@gmail.com>
2023-06-13 09:07:26 +0800

    finish spring mvc demo code without web part

---
+++ spring-mvc-demo/src/main/java/com/morgan/springIoCandDIdemo/config/ServletContainerInitConfig.java
@@ -0,0 +1,29 @@
+package com.morgan.springIoCandDIdemo.config;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class ServletContainerInitConfig extends AbstractDispatcherServletInitializer{
+
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(SpringMvcConfig.class);
+        return ctx;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+
+    }
+
+    @Override
+    @Nullable
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
+    
+}

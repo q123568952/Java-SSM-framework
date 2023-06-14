ade3d0711359d6c0aaf4217b640bdd06f04d5669
Morgan Huang <m800313@gmail.com>
2023-06-14 12:31:34 +0800

    add @requestparam to combine web param and type param

--- springmvc-request-param/src/main/java/com/morgan/springIoCandDIdemo/controller/UserController.java
+++ springmvc-request-param/src/main/java/com/morgan/springIoCandDIdemo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.morgan.springIoCandDIdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,6 +13,13 @@ public class UserController {
     public String commonParam(String name, int age){
         System.out.println("普通參數傳遞"+ name + "-----"+age);
         return "{'module':'common Param'}";
+    }
+
+     @RequestMapping("/commonParamDifferentName")
+    @ResponseBody
+    public String commonParamDifferentName(@RequestParam("name") String userName, int age){
+        System.out.println("普通參數傳遞"+ userName + "-----"+age);
+        return "{'module':'common Param different name'}";
     }
 
 }

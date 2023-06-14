12eaf6403518e122bae7b93996ca4caad4da23e4
Morgan Huang <m800313@gmail.com>
2023-06-14 10:07:55 +0800

    new module for request param demo

---
+++ springmvc-request-param/src/main/java/com/morgan/springIoCandDIdemo/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.morgan.springIoCandDIdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/save")
+    @ResponseBody
+    public String save(){
+        System.out.println("user save ...");
+        return "{'module':'user save'}";
+    }
+     @RequestMapping("/delete")
+    @ResponseBody
+    public String delete(){
+        System.out.println("user delete ...");
+        return "{'module':'user delete'}";
+    }
+}
---
+++ springmvc-request-param/src/main/java/com/morgan/springIoCandDIdemo/service/UserService.java
@@ -0,0 +1,7 @@
+package com.morgan.springIoCandDIdemo.service;
+
+import com.morgan.springIoCandDIdemo.domain.User;
+
+public interface UserService {
+    void save(User user);
+}

bea57242edfb18187f2315e87efc7bab7ad032d3
Morgan Huang <m800313@gmail.com>
2023-06-15 08:22:18 +0800

    add method for responding Json List data

--- springmvc-response/src/main/java/com/morgan/springIoCandDIdemo/controller/UserController.java
+++ springmvc-response/src/main/java/com/morgan/springIoCandDIdemo/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.morgan.springIoCandDIdemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,5 +32,20 @@ public class UserController {
         user.setAge(80);
         System.out.println("返回Json數據");
         return user;
+    }
+    @RequestMapping("/toJsonList")
+    @ResponseBody
+    public List<User> toJsonList(){
+        List<User> users = new ArrayList<>();
+        User user1 = new User();
+        user1.setName("888");
+        user1.setAge(80);
+        users.add(user1);
+        User user2 = new User();
+        user2.setName("777");
+        user2.setAge(90);
+        users.add(user2);
+        System.out.println("返回Json list數據");
+        return users;
     }
 }

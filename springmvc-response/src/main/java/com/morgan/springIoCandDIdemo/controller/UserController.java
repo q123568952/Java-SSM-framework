752e2826517437de66a8a7d67dfec09e2333ffb7
Morgan Huang <m800313@gmail.com>
2023-06-15 08:18:16 +0800

    add method for responding Json pojo

--- springmvc-response/src/main/java/com/morgan/springIoCandDIdemo/controller/UserController.java
+++ springmvc-response/src/main/java/com/morgan/springIoCandDIdemo/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.morgan.springIoCandDIdemo.domain.User;
+
 
 @Controller
 public class UserController {
@@ -18,5 +20,14 @@ public class UserController {
     public String toText(){
         System.out.println("返回文本數據");
         return "response text";
+    }
+    @RequestMapping("/toJsonPojo")
+    @ResponseBody
+    public User toJsonPojo(){
+        User user = new User();
+        user.setName("888");
+        user.setAge(80);
+        System.out.println("返回Json數據");
+        return user;
     }
 }

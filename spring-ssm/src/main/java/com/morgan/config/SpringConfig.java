16960dc011287561447178e5116cbf327f6ffbc4
Morgan Huang <m800313@gmail.com>
2023-06-16 23:00:26 +0800

    add mybatis and jdbc setting

--- spring-ssm/src/main/java/com/morgan/config/SpringConfig.java
+++ spring-ssm/src/main/java/com/morgan/config/SpringConfig.java
@@ -2,10 +2,13 @@ package com.morgan.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan({"com.morgan.service"})
-
+@PropertySource("jdbc.properties")
+@Import({JdbcConfig.class, MybatisConfig.class})
 public class SpringConfig {
     
 }

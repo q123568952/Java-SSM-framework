1b9d823031781682b3acc631682b547ec51629e4
Morgan Huang <m800313@gmail.com>
2023-06-17 22:14:56 +0800

    finish bookservice and bookserviceimpl setting

--- spring-ssm/src/main/java/com/morgan/controller/BookController.java
+++ spring-ssm/src/main/java/com/morgan/controller/BookController.java
@@ -1,5 +1,12 @@
 package com.morgan.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    
     
 }

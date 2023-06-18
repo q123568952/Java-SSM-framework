2b51dccdf1e90e324fbdc25961c241ac8369e016
Morgan Huang <m800313@gmail.com>
2023-06-18 15:20:38 +0800

    add transaction settin at springconfig and jdbcconfig

--- spring-ssm/src/main/java/com/morgan/config/JdbcConfig.java
+++ spring-ssm/src/main/java/com/morgan/config/JdbcConfig.java
@@ -4,6 +4,8 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
@@ -26,5 +28,11 @@ public class JdbcConfig {
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         return dataSource;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource){
+        DataSourceTransactionManager ds = new DataSourceTransactionManager();
+        ds.setDataSource(dataSource);
+        return ds;
     }
 }
--- spring-ssm/src/main/java/com/morgan/config/SpringConfig.java
+++ spring-ssm/src/main/java/com/morgan/config/SpringConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan({"com.morgan.service"})
 @PropertySource("classpath:jdbc.properties")
 @Import({JdbcConfig.class, MybatisConfig.class})
+@EnableTransactionManagement
 public class SpringConfig {
     
 }

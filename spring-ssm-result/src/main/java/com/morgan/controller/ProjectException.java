7e800f88d7384d10c5232464b004631cb1e4e17d
Morgan Huang <m800313@gmail.com>
2023-06-19 12:48:04 +0800

    finish exception sorting and define reply formate

--- spring-ssm-result/src/main/java/com/morgan/controller/ProjectException.java
+++ spring-ssm-result/src/main/java/com/morgan/controller/ProjectException.java
@@ -3,11 +3,29 @@ package com.morgan.controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.morgan.exception.BusinessException;
+import com.morgan.exception.SystemException;
+
 @RestControllerAdvice
 public class ProjectException {
 
+    @ExceptionHandler(SystemException.class)
+    public Result doSystemException(SystemException ex){
+        // record log
+        // send msg to ops engineer
+        // send email to develop engineer
+        return new Result(ex.getCode(),null,ex.getMessage());
+    }
+    @ExceptionHandler(BusinessException.class)
+    public Result doBusinessException(BusinessException ex){
+        return new Result(ex.getCode(),null,ex.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public Result doException(Exception ex){
-        return new Result(666,null,"error~~~~~");
+        // record log
+        // send msg to ops engineer
+        // send email to develop engineer
+        return new Result(Code.SYSTEM_UNKNOWN_ERR,null,"system error");
     }
 }

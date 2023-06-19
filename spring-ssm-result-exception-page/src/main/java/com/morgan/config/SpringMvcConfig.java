c417b59627adf1b2a859bbfa8b6807ffc0864b87
Morgan Huang <m800313@gmail.com>
2023-06-19 19:29:10 +0800

    add getall function but...I still need to learn web setting

--- spring-ssm-result-exception-page/src/main/java/com/morgan/config/SpringMvcConfig.java
+++ spring-ssm-result-exception-page/src/main/java/com/morgan/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.morgan.controller")
+@ComponentScan({"com.morgan.controller","com.morgan.config"})
 @EnableWebMvc
 public class SpringMvcConfig {
     

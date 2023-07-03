5aaa7cc601ce1b1c4ce7f6bf149585c4558abc58
Morgan Huang <m800313@gmail.com>
2023-07-03 15:02:49 +0800

    add interceptor functiom

--- spring-ssm-result-exception-page/src/main/java/com/morgan/config/SpringMvcSupport.java
+++ spring-ssm-result-exception-page/src/main/java/com/morgan/config/SpringMvcSupport.java
@@ -1,19 +1,31 @@
 package com.morgan.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.morgan.controller.interceptor.ProjectInterceptor;
+
+@Configuration
 public class SpringMvcSupport extends WebMvcConfigurationSupport{
+
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
-        registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/");
+        registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/");      
+    }
 
-            
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books","/books/*");
     }
     
 }
